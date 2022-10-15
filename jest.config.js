e5610a5cfcf5b7ec6e582d47050c97165a78adab
Adam Rafiandri <rafiandria23@gmail.com>
2022-10-15 20:45:03 +0700

    feat: add `uglify-js`

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    '^.+\\.ts?$': 'ts-jest',
   },
-  moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1"
-  }
 };
